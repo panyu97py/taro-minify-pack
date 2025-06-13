16c848e4d45c657a8e785243820d896280c4ecd8
yu.pan <panyupy@vip.qq.com>
2025-06-13 17:36:28 +0800

    feat: 修改实现

--- src/plugins/async-pack/index.ts
+++ src/plugins/async-pack/index.ts
@@ -5,6 +5,8 @@ import {transformWebpackRuntime} from './transform-webpack-runtime';
 import {TransformBeforeCompressionPlugin, PLUGIN_NAME, TransformOpt} from './transform-before-compression-plugin';
 import {DynamicPackOpts} from './types';
 import {transformAppStylesheet} from "./transform-app-stylesheet";
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const dynamicPackOptsDefaultOpt: DynamicPackOpts = {
   dynamicModuleJsDir: 'dynamic-common',
@@ -17,52 +19,68 @@ export default (ctx: IPluginContext, pluginOpts: DynamicPackOpts) => {
   if (process.env.TARO_ENV !== Taro.ENV_TYPE.WEAPP) return; // TODO 可能有大小写问题
 
   ctx.modifyWebpackChain(({chain}) => {
-    chain.optimization.merge({
-      splitChunks: {
-        cacheGroups: {
-          common: {
-            name: 'common',
-            minChunks: 2,
-            priority: 1,
-            enforce: true,
-          },
-          vendors: {
-            name: 'vendors',
-            minChunks: 2,
-            test: (module: any) => /[\\/]node_modules[\\/]/.test(module.resource),
-            priority: 10,
-            enforce: true,
-          },
-          [`${finalOpts.dynamicModuleJsDir}-js`]: {
-            name: (module: any) => `${finalOpts.dynamicModuleJsDir}/${module.buildInfo.hash}`,
-            chunks: 'async',
-            test: /\.(js|jsx|ts|tsx)$/,
-            enforce: true, // 强制拆分，即使 minSize 没达到
-          },
-          [`${finalOpts.dynamicModuleJsDir}-js-common`]: {
-            name: `${finalOpts.dynamicModuleJsDir}/common`,
-            chunks: 'async',
-            minChunks: 2,
-            priority: 2,
-            test: /\.(js|jsx|ts|tsx)$/,
-            enforce: true, // 强制拆分，即使 minSize 没达到
-          },
-          // TODO 待确认
-          // [`${finalOpts.dynamicModuleJsDir}-assets`]: {
-          //     name: `${finalOpts.dynamicModuleJsDir}/assets`,
-          //     chunks: 'async',
-          //     test: /\.(png|jpg|jpeg|svg)$/,
-          //     enforce: true, // 强制拆分，即使 minSize 没达到
-          // },
-          [`${finalOpts.dynamicModuleStyleFile}-css`]: {
-            name: finalOpts.dynamicModuleStyleFile,
-            chunks: 'async',
-            test: /\.(css|less|scss|sass)$/,
-            enforce: true, // 强制拆分，即使 minSize 没达到
-          },
-        },
+
+    // chain.optimization.merge({
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       common: {
+    //         name: 'common',
+    //         minChunks: 2,
+    //         priority: 1,
+    //         enforce: true,
+    //       },
+    //       vendors: {
+    //         name: 'vendors',
+    //         minChunks: 2,
+    //         test: (module: any) => /[\\/]node_modules[\\/]/.test(module.resource),
+    //         priority: 10,
+    //         enforce: true,
+    //       },
+    //       [`${finalOpts.dynamicModuleJsDir}-js`]: {
+    //         name: (module: any) => `${finalOpts.dynamicModuleJsDir}/${module.buildInfo.hash}`,
+    //         chunks: 'async',
+    //         test: /\.(js|jsx|ts|tsx)$/,
+    //         enforce: true, // 强制拆分，即使 minSize 没达到
+    //       },
+    //       [`${finalOpts.dynamicModuleJsDir}-js-common`]: {
+    //         name: `${finalOpts.dynamicModuleJsDir}/common`,
+    //         chunks: 'async',
+    //         minChunks: 2,
+    //         priority: 2,
+    //         test: /\.(js|jsx|ts|tsx)$/,
+    //         enforce: true, // 强制拆分，即使 minSize 没达到
+    //       },
+    //       // TODO 待确认
+    //       // [`${finalOpts.dynamicModuleJsDir}-assets`]: {
+    //       //     name: `${finalOpts.dynamicModuleJsDir}/assets`,
+    //       //     chunks: 'async',
+    //       //     test: /\.(png|jpg|jpeg|svg)$/,
+    //       //     enforce: true, // 强制拆分，即使 minSize 没达到
+    //       // },
+    //       [`${finalOpts.dynamicModuleStyleFile}-css`]: {
+    //         name: finalOpts.dynamicModuleStyleFile,
+    //         chunks: 'async',
+    //         test: /\.(css|less|scss|sass)$/,
+    //         enforce: true, // 强制拆分，即使 minSize 没达到
+    //       },
+    //     },
+    //   },
+    // });
+
+    chain.merge({
+      output: {
+        chunkFilename: 'async/[contenthash].js', // 异步模块输出路径
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
       },
-    });
+    })
+
+    chain.plugin('miniCssExtractPlugin')
+      .tap((args: [MiniCssExtractPlugin.PluginOptions]) => {
+        const [options] = args;
+        const finalOption = {...options, chunkFilename: finalOpts.dynamicModuleStyleFile}
+        return [finalOption];
+      });
 
     chain.plugin(PLUGIN_NAME).use(TransformBeforeCompressionPlugin, [{
       test: /^(runtime\.js|app\.wxss)$/,
