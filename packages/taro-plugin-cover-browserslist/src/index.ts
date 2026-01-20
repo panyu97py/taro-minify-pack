8c3aa2357566b2f60f85faccffa96e662ae71c0a
yu.pan <panyupy@vip.qq.com>
2026-01-20 11:55:11 +0800

    feat: 新增 @taro-minify-pack/plugin-cover-browserslist

---
+++ packages/taro-plugin-cover-browserslist/src/index.ts
@@ -0,0 +1,20 @@
+import { getBrowsersList } from 'miniprogram-compat'
+import type { IPluginContext } from '@tarojs/service'
+
+export interface CoverBrowsersListOpt {
+    minBaseLibraryVersion: string
+}
+
+export default (ctx: IPluginContext, pluginOpts:CoverBrowsersListOpt) => {
+  ctx.onBuildStart(() => {
+    if (process.env.TARO_ENV !== 'weapp' || !pluginOpts.minBaseLibraryVersion) return
+
+    const browsersList = getBrowsersList(pluginOpts.minBaseLibraryVersion)
+
+    if (!browsersList) return
+
+    process.env.BROWSERSLIST = browsersList?.join(',')
+
+    console.log('✅ Successfully set BROWSERSLIST from minBaseLibraryVersion')
+  })
+}
--- packages/taro-preset/src/index.ts
+++ packages/taro-preset/src/index.ts
@@ -1,17 +1,20 @@
 import { RemoteAssetPluginOpt } from '@taro-minify-pack/plugin-remote-assets'
 import { AsyncPackOpts } from '@taro-minify-pack/plugin-async-pack'
+import { CoverBrowsersListOpt } from '@taro-minify-pack/plugin-cover-browserslist'
 
 export * from '@taro-minify-pack/plugin-remote-assets'
 
 interface Opt {
     remoteAssets?: RemoteAssetPluginOpt
     asyncPack?: AsyncPackOpts | boolean
+    coverBrowsersList: CoverBrowsersListOpt
 }
 
 export default (_: any, opt: Opt) => {
-  const { remoteAssets, asyncPack } = opt
+  const { remoteAssets, asyncPack, coverBrowsersList } = opt
   const plugins = []
   if (remoteAssets) plugins.push([require.resolve('@taro-minify-pack/plugin-remote-assets'), remoteAssets])
+  if (coverBrowsersList) plugins.push([require.resolve('@taro-minify-pack/plugin-cover-browserslist'), coverBrowsersList])
   if (asyncPack && typeof asyncPack === 'boolean') plugins.push(require.resolve('@taro-minify-pack/plugin-async-pack'))
   if (asyncPack && typeof asyncPack !== 'boolean') plugins.push([require.resolve('@taro-minify-pack/plugin-async-pack'), asyncPack])
   return { plugins }
