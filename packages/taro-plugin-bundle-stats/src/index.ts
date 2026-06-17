c28235dcfd8d2f7d7615fcb9d3cc7f81749125de
yu.pan <panyupy@vip.qq.com>
2026-06-17 18:09:41 +0800

    feat: 调整配置

--- packages/taro-plugin-bundle-stats/src/index.ts
+++ packages/taro-plugin-bundle-stats/src/index.ts
@@ -1,10 +1,29 @@
 import type { IPluginContext } from '@tarojs/service'
 import { BundleStatsWebpackPlugin } from 'bundle-stats-webpack-plugin'
 import { BundleStatsOpt } from './types'
+import path from 'path'
 
-export default (ctx: IPluginContext, pluginOpts: BundleStatsOpt = {}) => {
+const bundleStatsDefaultOpt: BundleStatsOpt = {
+  reportPath: 'bundleStatsReport',
+  baselinePath: 'bundleStatsReport/baseline.json'
+}
+export default (ctx: IPluginContext, pluginOpts: Partial<BundleStatsOpt> = {}) => {
+  const finalOpts:BundleStatsOpt = { ...bundleStatsDefaultOpt, ...pluginOpts }
+
   ctx.modifyWebpackChain(({ chain }) => {
-    const finalOpts = { ...pluginOpts, defaultSizes: 'parsed', analyzerMode: 'server' } as BundleStatsWebpackPlugin.Options
-    chain.plugin('stats').use(BundleStatsWebpackPlugin, [finalOpts])
+    const { appPath, outputPath } = ctx.paths
+    const { reportPath, ...rest } = finalOpts
+    const outDir = path.relative(outputPath, path.resolve(appPath, reportPath))
+    const baselineFilepath = path.resolve(appPath, reportPath, 'baseline.json')
+    const config: BundleStatsWebpackPlugin.Options = { ...rest, baselineFilepath, outDir }
+    chain.plugin('bundle-stats').use(BundleStatsWebpackPlugin, [config])
+  })
+
+  ctx.onBuildStart(() => {
+    // 加载baseline文件
+  })
+
+  ctx.onBuildComplete(() => {
+    // 总结报告
   })
 }
--- packages/taro-plugin-bundle-stats/src/types.ts
+++ packages/taro-plugin-bundle-stats/src/types.ts
@@ -1,3 +1,46 @@
-export interface BundleStatsOpt {
+interface WebpackStatsOptions {
+    /**
+     * Output webpack assets information
+     * Default: `true`.
+     */
+    assets?: boolean;
 
+    /**
+     * Output webpack chunks information
+     * Default: `true`.
+     */
+    chunks?: boolean;
+
+    /**
+     * Output webpack modules information
+     * Default: `true`.
+     */
+    modules?: boolean;
+
+    /**
+     * Output webpack hash information
+     * Default: `true`.
+     */
+    hash?: boolean;
+
+    /**
+     * Output webpack builtAt information
+     * Default: `true`.
+     */
+    builtAt?: boolean;
+}
+
+export interface BundleStatsOpt {
+    html?: boolean;
+    json?: boolean;
+    silent?: boolean;
+    compare?: boolean;
+    baseline?: boolean;
+    baselinePath?: string;
+    reportPath: string;
+    /**
+     * webpack stats options
+     * Default: `{ assets: true: true, chunks: true, modules: true, hash: true, builtAt: true }`.
+     */
+    stats?: Partial<WebpackStatsOptions>;
 }
